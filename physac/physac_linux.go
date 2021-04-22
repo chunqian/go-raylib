71920e8a881a4ff60334ed561c382501e97bf796
chunqian.shen <chunqian.shen@qq.com>
2021-04-22 22:43:32 +0800

    fix physac binding

--- physac/physac_linux.go
+++ physac/physac_linux.go
@@ -4,6 +4,7 @@ package physac
 
 /*
 // #cgo linux LDFLAGS: -L${SRCDIR}/../lib/raylib/src -lraylib -lGL -lm -pthread -ldl -lrt -lX11
+#cgo linux LDFLAGS: -lGL -lm -pthread -ldl -lrt -lX11
 #cgo linux CFLAGS: -I../lib/raylib/src -DPHYSAC_IMPLEMENTATION -DPHYSAC_STATIC -DPHYSAC_NO_THREADS
 */
 import "C"
--- physac/raylib_windows.go
+++ physac/raylib_windows.go
@@ -4,6 +4,7 @@ package physac
 
 /*
 // #cgo windows LDFLAGS: -L${SRCDIR}/../lib/raylib/src -lraylib -lopengl32 -lgdi32 -lwinmm -lole32
+#cgo windows LDFLAGS: -lopengl32 -lgdi32 -lwinmm -lole32
 #cgo windows CFLAGS: -I../lib/raylib/src -DPHYSAC_IMPLEMENTATION -DPHYSAC_STATIC -DPHYSAC_NO_THREADS
 */
 import "C"
